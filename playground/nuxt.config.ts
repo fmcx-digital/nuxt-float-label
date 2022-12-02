201aa5020cba8bf5d57a379eb705620777a74b1f
Lucas Simines <lucas@fmcx.digital>
2022-12-02 13:21:50 -0300

    fix(playground): deploy

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,18 +2,12 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
   modules: ['../src/module', '@nuxtjs/tailwindcss', '@nuxt/content'],
-  ssr: false,
   css: [
     '@/assets/css/fonts.css',
     '@/assets/css/base.css',
     '@/assets/fonts/fontawesome/css/fontawesome.min.css',
     '@/assets/fonts/fontawesome/css/regular.min.css'
   ],
-  nitro: {
-    prerender: {
-      routes: ['/']
-    }
-  },
   postcss: {
     plugins: {
       autoprefixer: {},
