403901c9e402ac68173b0fbd2e89e1ea451389be
Lucas Simines <lucas@fmcx.digital>
2022-12-03 10:47:05 -0300

    style(playground):

--- playground/components/HeaderUi.vue
+++ playground/components/HeaderUi.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="bg-slate-900 h-[4.5rem]">
+  <header class="bg-slate-900 h-[4.5rem] mobile-max:h-[58px]">
     <div class="flex justify-center items-center h-full">
-      <img src="/fmcx.svg" class="block w-[90px]" />
+      <img src="/fmcx.svg" class="block w-[90px] mobile-max:w-[72px]" />
     </div>
   </header>
 </template>
